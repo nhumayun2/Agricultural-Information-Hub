78d1a124bc541d8a93ba1a6f4318e26fa785368a
Humayun Ahmed <humayunahmedniloy1@gmail.com>
2025-05-16 00:20:58 +0600

    crop portion complete

---
+++ src/Interfaces/cropType.ts
@@ -0,0 +1,8 @@
+export interface cropType {
+  name: string;
+  image: string;
+  season: string;
+  soil: string;
+  diseases: string[];
+  tips: string[];
+}
--- src/app/pages/crop-details/crop-details.component.ts
+++ src/app/pages/crop-details/crop-details.component.ts
@@ -1,23 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import cropsData from '../../../assets/crops.json';
+import { cropType } from '../../../Interfaces/cropType';
 
 @Component({
   selector: 'app-crop-details',
   imports: [CommonModule],
   templateUrl: './crop-details.component.html',
-  styleUrl: './crop-details.component.css'
+  styleUrl: './crop-details.component.css',
 })
-export class CropDetailsComponent {
-showCategories = false;
+export class CropDetailsComponent implements OnInit {
+  showCategories = false;
 
-  cropCategories: string[] = [
-    'Grains (Rice, Wheat)',
-    'Vegetables (Carrot, Potato)',
-    'Fruits (Mango, Banana)',
-    'Oilseeds (Mustard, Sunflower)'
-  ];
+  crops: cropType[] = [];
 
   toggleCategories() {
     this.showCategories = !this.showCategories;
+  }
+
+  ngOnInit(): void {
+    this.crops = cropsData;
   }
 }
