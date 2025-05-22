41556cec3b7d8b3792c1cb707edd2cc8b546f000
nhumayun2 <humayunahmedniloy1@gmail.com>
2025-05-23 00:57:21 +0600

    completed pest control

--- src/app/pages/pest-control/pest-control.component.ts
+++ src/app/pages/pest-control/pest-control.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import pestData from '../../../assets/pests.json'
-import { pestType } from '../../../Interfaces/pestType'
+import pestData from '../../../assets/pests.json';
+import { pestType } from '../../../Interfaces/pestType';
 
 @Component({
   selector: 'app-pest-control',
   imports: [CommonModule],
   templateUrl: './pest-control.component.html',
-  styleUrl: './pest-control.component.css'
+  styleUrl: './pest-control.component.css',
 })
 export class PestControlComponent implements OnInit {
-  pestData: pestType[] = []
+  pestData: pestType[] = [];
   ngOnInit(): void {
     this.pestData = pestData;
+  }
+  selectedImage: string | null = null;
+
+  openImage(imageUrl: string) {
+    this.selectedImage = imageUrl;
+  }
+
+  closeImage() {
+    this.selectedImage = null;
   }
 }
