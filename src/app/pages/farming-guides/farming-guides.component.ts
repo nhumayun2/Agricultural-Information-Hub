4eee6039bde8f19374d7c32eaa8dfdee71757641
nhumayun2 <humayunahmedniloy1@gmail.com>
2025-05-23 15:46:40 +0600

    added "allowedCommonJsDependencies": ["aos"] to angular.json

--- src/app/pages/farming-guides/farming-guides.component.ts
+++ src/app/pages/farming-guides/farming-guides.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { farming_guide_type } from '../../../Interfaces/farming-guide-type';
 import farmingGuides from '../../../assets/farming-guides.json';
 
 @Component({
   selector: 'app-farming-guides',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './farming-guides.component.html',
   styleUrl: './farming-guides.component.css',
 })
